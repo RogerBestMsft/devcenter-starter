d9c00e9c27877c579b1023bd6a0bc369e19aee67
Roger Best <rbest@microsoft.com>
2024-05-03 14:06:10 -0700

    Try 6

--- devCenter/setup.sh
+++ devCenter/setup.sh
@@ -56,11 +56,6 @@ echo "Enable DevCenter cli extension"
 az extension add --name devcenter --allow-preview true
 echo "... done"
 
-echo "test secret"
-echo $SECRETS
-echo $($SECRETS | jq -rc )
-echo "... done"
-
 echo "Check for existance of Devcenter: $DEVCENTERNAME"
 if [ $(az devcenter admin devcenter list --resource-group $RESOURCEGROUPNAME --subscription $SUBSCRIPTIONID --query "[?name=='$DEVCENTERNAME']") ]; then
 
@@ -74,7 +69,7 @@ if [ $(az devcenter admin devcenter list --resource-group $RESOURCEGROUPNAME --s
 		--parameters \
 			config=@$CONFIGFILE \
 			resolve=$RESOLVE \
-			secrets=$SECRETS \
+			secrets=$($SECRETS | jq .) \
 			windows365PrincipalId=$(az ad sp show --id 0af06dc6-e4b5-4f28-818e-e78e62d137a5 --query id --output tsv | dos2unix) \
 		--query properties.outputs > ${CONFIGFILE%.*}.output.json && echo "... done"
 
