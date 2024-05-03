b3e01d8dd312c0ec7b87c142eb463bb1346beded
Roger Best <rbest@microsoft.com>
2024-05-03 14:33:31 -0700

    d441

--- devCenter/setup.sh
+++ devCenter/setup.sh
@@ -69,7 +69,7 @@ if [ $(az devcenter admin devcenter list --resource-group $RESOURCEGROUPNAME --s
 		--parameters \
 			config=@$CONFIGFILE \
 			resolve=$RESOLVE \
-			secrets=$($SECRETS | jq .) \
+			secrets=$SECRETS \
 			windows365PrincipalId=$(az ad sp show --id 0af06dc6-e4b5-4f28-818e-e78e62d137a5 --query id --output tsv | dos2unix) \
 		--query properties.outputs > ${CONFIGFILE%.*}.output.json && echo "... done"
 
