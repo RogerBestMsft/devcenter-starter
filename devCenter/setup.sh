963e2a1038cfc44cb89b14eb29e4240c65cc6b77
Roger Best <rbest@microsoft.com>
2024-05-13 15:32:52 -0700

    add 1 2

--- devCenter/setup.sh
+++ devCenter/setup.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-usage() { 
-	echo "======================================================================================"
-	echo "Usage: $0"
-	echo "======================================================================================"
-	echo " -c [REQUIRED] 	Config file"
-	echo " -s [REQUIRED]	SECRETSFILE"
-	exit 1; 
-}
+# usage() { 
+# 	echo "======================================================================================"
+# 	echo "Usage: $0"
+# 	echo "======================================================================================"
+# 	echo " -c [REQUIRED] 	Config file"
+# 	echo " -s [REQUIRED]	SECRETSFILE"
+# 	exit 1; 
+# }
 
-while getopts 'c:s' OPT; do
-    case "$OPT" in
-		c)
-			CONFIGFILE="${OPTARG}" ;;
-		s)
-			SECRETSFILE="${OPTARG}" ;;
-		*) 
-			usage ;;
-    esac
-done
+# while getopts 'c:s' OPT; do
+#     case "$OPT" in
+# 		c)
+# 			CONFIGFILE="${OPTARG}" ;;
+# 		s)
+# 			SECRETSFILE="${OPTARG}" ;;
+# 		*) 
+# 			usage ;;
+#     esac
+# done
 
 echo "Generating data files ..."; mkdir -p ./data
 
@@ -37,17 +37,17 @@ echo "... done"
 
 echo "Deleting output files ..."
 rm -f ./deploy.output.json
-rm -f ${CONFIGFILE%.*}.output.json
+rm -f ${1%.*}.output.json
 echo "... done"
 
 echo "Checking for DevCenter"
-SUBSCRIPTIONID=$(jq --raw-output .subscription $CONFIGFILE)
-DEVCENTERNAME=$(jq --raw-output .name $CONFIGFILE)
-LOCATION=$(jq --raw-output .location $CONFIGFILE)
+SUBSCRIPTIONID=$(jq --raw-output .subscription $1)
+DEVCENTERNAME=$(jq --raw-output .name $1)
+LOCATION=$(jq --raw-output .location $1)
 echo "Location $LOCATION"
 
 
-echo "Deploying DevCenter '$CONFIGFILE' ..."
+echo "Deploying DevCenter '$1' ..."
 az deployment sub create \
 	--subscription "$SUBSCRIPTIONID" \
 	--name $(uuidgen) \
@@ -55,8 +55,8 @@ az deployment sub create \
 	--template-file ./main.bicep \
 	--only-show-errors \
 	--parameters \
-		config=@$CONFIGFILE \
+		config=@$1 \
 		resolve=$RESOLVE \
-		secrets=@$(echo $SECRETSFILE) \
+		secrets=@$(echo $2) \
 		windows365PrincipalId=$(az ad sp show --id 0af06dc6-e4b5-4f28-818e-e78e62d137a5 --query id --output tsv | dos2unix) \
-	--query properties.outputs > ${CONFIGFILE%.*}.output.json && echo "... done"
+	--query properties.outputs > ${1%.*}.output.json && echo "... done"
