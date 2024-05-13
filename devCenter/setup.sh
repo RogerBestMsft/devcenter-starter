876ca6258242860ab422af82e782a212e78ea69b
Roger Best <rbest@microsoft.com>
2024-05-13 13:27:33 -0700

    use jq

--- devCenter/setup.sh
+++ devCenter/setup.sh
@@ -22,8 +22,12 @@ done
 
 echo "Generating data files ..."; mkdir -p ./data
 
-echo "Check SecretsFile"
-#[ -f $SECRETSFILE ] || (echo "{}" > $SECRETSFILE)
+echo "Check SecretsFile" 
+[ -f $SECRETSFILE ] || (echo "{}" > $SECRETSFILE)
+echo "x $SECRETSFILE"
+
+TEST=$(jq --raw-output $SECRETSFILE)
+echo "y $TEST"
 
 az account list-locations --query '[].{key: name, value: displayName}' | jq 'map( { (.key): .value }) | add' > ./data/locations.json
 az role definition list --query '[].{ key: roleName, value: name}' | jq 'map( { (.key | gsub("\\s+";"") | ascii_downcase): .value }) | add' > ./data/roles.json
@@ -49,6 +53,6 @@ az deployment sub create \
 	--parameters \
 		config=@$CONFIGFILE \
 		resolve=$RESOLVE \
-		secrets=@$SECRETSFILE \
+		secrets=@$TEST \
 		windows365PrincipalId=$(az ad sp show --id 0af06dc6-e4b5-4f28-818e-e78e62d137a5 --query id --output tsv | dos2unix) \
 	--query properties.outputs > ${CONFIGFILE%.*}.output.json && echo "... done"
