602b3e6df08906a4a905a862f4b4bacb933b5177
Roger Best <rbest@microsoft.com>
2024-05-13 13:31:12 -0700

    toJSOn

--- devCenter/setup.sh
+++ devCenter/setup.sh
@@ -23,7 +23,7 @@ done
 echo "Generating data files ..."; mkdir -p ./data
 
 echo "Check SecretsFile" 
-[ -f $SECRETSFILE ] || (echo "{}" > $SECRETSFILE)
+#[ -f $SECRETSFILE ] || (echo "{}" > $SECRETSFILE)
 echo "x $SECRETSFILE"
 
 TEST=$(jq --raw-output $SECRETSFILE)
@@ -53,6 +53,6 @@ az deployment sub create \
 	--parameters \
 		config=@$CONFIGFILE \
 		resolve=$RESOLVE \
-		secrets=@$TEST \
+		secrets=@$SECRETSFILE \
 		windows365PrincipalId=$(az ad sp show --id 0af06dc6-e4b5-4f28-818e-e78e62d137a5 --query id --output tsv | dos2unix) \
 	--query properties.outputs > ${CONFIGFILE%.*}.output.json && echo "... done"
